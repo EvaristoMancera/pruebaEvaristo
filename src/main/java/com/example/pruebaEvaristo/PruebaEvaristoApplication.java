d5a92759e61e9d3e0bf8884fbfc5071099282367
EvaristoMancera <evaristomancerallano@gmail.com>
2023-09-28 18:01:12 +0200

    prueba subida
    prueba de subida

---
+++ src/main/java/com/example/pruebaEvaristo/PruebaEvaristoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebaEvaristo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaEvaristoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaEvaristoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebaEvaristo/PruebaEvaristoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebaEvaristo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaEvaristoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
